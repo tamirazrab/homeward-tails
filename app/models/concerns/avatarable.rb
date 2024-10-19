3824f3fad416f66d2ee2d61f1c053b15f12799fe
Edwin Mak <edwinthinks@gmail.com>
2024-10-19 11:41:52 +0200

    Skip over the need for Azure to be defined when doing precomppile

--- app/models/concerns/avatarable.rb
+++ app/models/concerns/avatarable.rb
@@ -2,12 +2,14 @@ module Avatarable
   extend ActiveSupport::Concern
 
   included do
-    has_one_attached :avatar
+    unless ENV["SECRET_KEY_BASE_DUMMY"].present?
+      has_one_attached :avatar
 
-    # TODO:  move these validation strings to a locale file
-    validates :avatar, content_type: {in: ["image/png", "image/jpeg"],
-                                      message: "must be PNG or JPEG"},
-      size: {between: 10.kilobyte..1.megabytes,
-             message: "size must be between 10kb and 1Mb"}
+      # TODO:  move these validation strings to a locale file
+      validates :avatar, content_type: {in: ["image/png", "image/jpeg"],
+        message: "must be PNG or JPEG"},
+        size: {between: 10.kilobyte..1.megabytes,
+          message: "size must be between 10kb and 1Mb"}
+    end
   end
 end
