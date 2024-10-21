4e046e55f47443df2f9a510c0d88c1d63a534f5f
Yuji K. <yuji.k.dev@gmail.com>
2024-10-21 16:46:54 -0600

    Change org/home create account button to sing up and style change (#1075)

--- test/system/home_page_test.rb
+++ test/system/home_page_test.rb
@@ -7,11 +7,16 @@ class HomePageTest < ApplicationSystemTestCase
     set_organization(@organization)
   end
 
-  test "renders custom hero and about text from CustomPage or default text" do
+  test "renders custom hero and about text from CustomPage or default text, checks cta button link" do
     CustomPage.create(hero: "Super Pets for a good paws", about: "All about us")
 
     visit home_index_path
     assert_text "Super Pets for a good paws"
     assert_text "All about us"
+    assert_selector "#cta"
+
+    within "#cta" do
+      assert_selector "a", text: "Sign Up"
+    end
   end
 end
